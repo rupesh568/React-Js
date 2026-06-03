10a840f89559e91fe29f96f26762ff5291b37369
Rupesh Kumar Chaudhary <rupeshjaiswal999939>
2026-06-03 09:37:30 +0530

    React-Router

--- projects/46-react-router/src/main.jsx
+++ projects/46-react-router/src/main.jsx
@@ -1,11 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import {RouterProvider,createBrowserRouter} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App.jsx'
-
+import App from './routes/App.jsx';
+import CreatePost from './Components/CreatePost.jsx';
+import PostList from './Components/PostList.jsx';
+const router=createBrowserRouter([
+  {path:"/",
+    element:<App/>,
+    children:[
+        {path:"/",element:<PostList/>},
+        {path:"/create-post",element:<CreatePost/>}
+    ],
+  
+  }
+  
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
+    
   </StrictMode>,
 )
--- projects/46-react-router/src/routes/App.jsx
+++ projects/46-react-router/src/routes/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./App.css";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import Sidebar from "./Components/Sidebar";
-import CreatePost from "./Components/CreatePost";
-import Post from "./Components/Post";
-import PostList from "./Components/PostList";
-import PostListProvider from "./Store/Post-list-store";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import Sidebar from "../Components/Sidebar";
+import CreatePost from "../Components/CreatePost";
+import Post from "../Components/Post";
+import PostList from "../Components/PostList";
+import PostListProvider from "../Store/Post-list-store";
+import { Outlet } from "react-router-dom";
 
 function App() {
     const [selectedTab, setSelectedTab] = useState("Home");
@@ -20,12 +21,12 @@ function App() {
             ></Sidebar>
             <div className="Container">
                 <Header></Header>
-                {selectedTab === "Home" ? (
+                {/* {selectedTab === "Home" ? (
                     <PostList></PostList>
                 ) : (
                     <CreatePost></CreatePost>
-                )}
-
+                )} */}
+                <Outlet></Outlet>
                 <Footer></Footer>
             </div>
         </div>
